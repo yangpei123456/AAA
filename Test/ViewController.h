cb627d55f78b378ab0a1fe553963291b624fa920
YANGPEI <15172338506@163.com>
2016-02-08 17:19:01 +0800

    addTest2016/2/7/1

---
+++ Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by caozhenwei on 16/2/8.
+//  Copyright © 2016年 caozhenwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Test/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  Test
+//
+//  Created by caozhenwei on 16/2/8.
+//  Copyright © 2016年 caozhenwei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    NSLog(@"----测试----");
+}
+
+
+@end
